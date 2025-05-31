df79bd68585e4d425ee4f5fa6112367b7309af46
FrankiNarvaez <frannki03@gmail.com>
2025-05-30 22:40:15 -0500

    feat: Post entity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -22,6 +23,8 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('administrador'),
         ]);
 
-        Category::factory(5)->create();
+        Category::factory(10)->create();
+
+        Post::factory(100)->create();
     }
 }
