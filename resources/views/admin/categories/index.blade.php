d1e1bbc36f8b269fc3b81f9558dec525ad99e562
FrankiNarvaez <frannki03@gmail.com>
2025-05-30 20:14:25 -0500

    feat: category view

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,47 @@
+<x-layouts.app>
+    <div class="flex justify-between items-center">
+        <flux:breadcrumbs>
+            <flux:breadcrumbs.item :href="route('dashboard')">
+                Dashboard
+            </flux:breadcrumbs.item>
+            <flux:breadcrumbs.item>
+                Categories
+            </flux:breadcrumbs.item>
+        </flux:breadcrumbs>
+        <a href="{{route('admin.categories.create') }}" class="btn btn-blue">New category</a>
+    </div>
+
+    <div class="relative overflow-x-auto mt-6">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $category->id }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $category->name }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="{{route('admin.categories.create') }}" class="btn btn-alt">Edit</a>
+                            <a href="{{route('admin.categories.create') }}" class="btn btn-red">Remove</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-layouts.app>
